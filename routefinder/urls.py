9d5e0dad3ea4f19c0798f572d44aed7bfa2bc74d
Muhammad Haris <mh0210053@gmail.com>
2025-12-05 20:53:22 +0500

    Route Finder: Muhammad Haris

---
+++ routefinder/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('routes',views.routes,name='routes'),
+    path('contribute',views.contribute,name='contribute'),
+    path('contact',views.contact,name='contact'),
+    path('home',views.home,name='home'),
+    path('stations/',views.station_search,name='station_search'),
+    path('find_route',views.find_route,name='find_route'),
+    path('map',views.map,name='map'),
+    path('manzil-admin/reports', views.admin_reports, name='admin_reports'),
+    path('manzil-admin/login', views.admin_login, name='admin_login'),
+    path('manzil-admin/logout', views.admin_logout, name='admin_logout'),
+]
